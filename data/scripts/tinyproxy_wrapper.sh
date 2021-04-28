2c7f964aaab50bf36ffd7aa243ebdab31715c807
fantik11 <35728123+fantik11@users.noreply.github.com>
2021-04-28 04:44:16 +0300

    Update tinyproxy_wrapper.sh

--- data/scripts/tinyproxy_wrapper.sh
+++ data/scripts/tinyproxy_wrapper.sh
@@ -8,7 +8,7 @@ until ping -c 3 1.1.1.1 > /dev/null 2>&1; do
     sleep 1
 done
 
-addr_eth=$(ip a show dev eth0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
+addr_eth=0.0.0.0
 addr_tun=$(ip a show dev tun0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
 sed -i \
     -e "/Listen/c Listen $addr_eth" \
